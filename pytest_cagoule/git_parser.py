9a731b981a31bae1e50fbbc11a4489904fc37d54
David Szotten <davidszotten@gmail.com>
2014-10-11 20:30:47 +0100

    git diff parser tests

--- pytest_cagoule/git_parser.py
+++ pytest_cagoule/git_parser.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import re
+import subprocess
 
 import fileinput
 stream = fileinput.input()
-# for line in
+
 
 def get_next_marker(stream):
     for line in stream:
@@ -27,22 +28,36 @@ def get_next_marker(stream):
             return match.groupdict()
 
 
-filename = None
-while True:
-    try:
-        marker = get_next_marker(stream)
-        if marker is None:
+def get_diff_changes(diff):
+    filename = None
+    stream = iter(diff.splitlines())
+    while True:
+        # we could probably just not catch StopIteration, and rely on it
+        # bubbling to stop the iteration over get_changes higher up
+        # too tricky?
+        try:
+            marker = get_next_marker(stream)
+            if marker is None:
+                break
+
+            if isinstance(marker, basestring):
+                filename = marker
+            elif isinstance(marker, dict):
+                del_start = int(marker['del_start'])
+                del_count = int(marker['del_count'] or 1)
+                del_end = del_start + del_count
+                yield filename, del_start, del_end
+
+        except StopIteration:
             break
 
-        if isinstance(marker, basestring):
-            filename = marker
-        elif isinstance(marker, dict):
-            del_start = int(marker['del_start'])
-            del_count = int(marker['del_count'] or 1)
-            print '{}:{}-{}'.format(
-                filename, del_start, del_start + del_count)
 
-    except StopIteration:
-        break
+def get_changes():
+    diff = subprocess.check_output(['git', 'diff', '--unified=0'])
+    for filename, start, end in get_diff_changes(diff):
+        yield filename, start, end
 
 
+if __name__ == '__main__':
+    for filename, start, end in get_changes():
+        print '{}:{}-{}'.format(filename, start, end)
---
+++ tests/test_git_parser.py
@@ -0,0 +1,44 @@
+from pytest_cagoule.git_parser import get_diff_changes
+
+
+diff1 = """diff --git a/README.rst b/README.rst
+index 8902dc2..9de8bf2 100644
+--- a/README.rst
++++ b/README.rst
+@@ -3,0 +4 @@ pytest-cagoule
++"""
+
+diff2 = """diff --git a/README.rst b/README.rst
+index 8902dc2..f39170e 100644
+--- a/README.rst
++++ b/README.rst
+@@ -3,0 +4 @@ pytest-cagoule
++
+@@ -29,4 +28,0 @@ Install **cagoule** using ``pip``::
+-License
+--------
+-
+-MIT. See ``LICENSE`` for details
+diff --git a/setup.py b/setup.py
+index bbe47a8..e83a1a7 100644
+--- a/setup.py
++++ b/setup.py
+@@ -0,0 +1 @@
++"""
+
+
+def test_get_diff_changes_simple():
+    expected = (
+        ('README.rst', 3, 3),
+    )
+    assert tuple(get_diff_changes(diff1)) == expected
+
+
+
+def test_get_diff_changes_multiple():
+    expected = (
+        ('README.rst', 3, 3),
+        ('README.rst', 29, 33),
+        ('setup.py', 0, 0),
+    )
+    assert tuple(get_diff_changes(diff2)) == expected
