af47ecd119b583e432bae06c947489b7b0a43581
David Szotten <davidszotten@gmail.com>
2020-01-01 21:11:15 +0000

    version bump

--- setup.py
+++ setup.py
@@ -41,10 +41,8 @@ setup(
         'Topic :: Software Development :: Testing',
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.7',
     ],
 
 )
